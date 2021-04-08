f194b59d9950fa8a19daed2d4a796a6ebc1c3309
ReklezWlthr <napatwattanasuttiwong@gmail.com>
2021-04-09 01:26:03 +0700

    [EDIT] Like Button and Put API

--- src/components/SongThumbnail.vue
+++ src/components/SongThumbnail.vue
@@ -1,11 +1,37 @@
 <template>
-    <div class="flex-col">
-        <div><img :src="require(`../assets/albumArts/${song.artist}-${song.name}.jpg`)"></div>
+  <div class="flex py-3 px-6">
+    <div>
+      <img
+        class="rounded-3xl w-36"
+        :src="require(`../assets/albumArts/${song.artist}-${song.name}.jpg`)"
+      />
     </div>
+    <div class="pl-3 pt-2">
+        <ul class="text-paleViolet">
+          <li class="font-semibold text-2xl">{{ song.name }}</li>
+          <li>{{ song.artist }}</li>
+          <li>{{ song.album }}</li>
+          <li @click="like(song)"><i :class="song.liked ? likeClass : dislikeClass "></i></li>
+        </ul>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    props:["song"]
-}
+    data(){
+        return {
+            likeClass: "fas fa-heart text-red-500 text-2xl pt-2",
+            dislikeClass: "fas fa-heart text-red-200 text-2xl pt-2 opacity-25"
+        }
+    },
+  props: ["song"],
+  emits: ["like-song"],
+  methods: {
+      like(song){
+          song.liked = !song.liked;
+          this.$emit("like-song", song);
+      }
+  }
+};
 </script>
--- src/views/SongList.vue
+++ src/views/SongList.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="navButton">Total Songs: {{ songList.length }}</div>
-  <div v-for="song in songList" :key="song.id">
-    <song-thumbnail :song="song" />
+  <div class="w-full flex" v-for="song in songList" :key="song.id">
+    <song-thumbnail :song="song" @like-song="editSong" />
   </div>
 </template>
 
@@ -23,11 +23,22 @@ export default {
       // parses JSON response into native JavaScript objects
       return data;
     },
+    async editSong(newSong) {
+      const res = await fetch(`${this.url}/${newSong.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(newSong),
+      });
+      const data = await res.json();
+      this.songList = this.songList.map(song => song.id == data.id ? data : song);
+    },
   },
   async created() {
     this.songList = await this.fetchSongs();
-    for(const song in this.$songList){
-        console.log(song.name)
+    for (const song in this.$songList) {
+      console.log(song.name);
     }
   },
 };
