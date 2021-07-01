24318ddd676d7b5ca9c6e70b44ae0be5b81d78e1
Wlthr <napat.watt@mail.kmutt.ac.th>
2021-07-02 01:50:34 +0700

    [EDIT] Filter Heroes

--- src/views/HeroList.vue
+++ src/views/HeroList.vue
@@ -5,6 +5,27 @@
       class="input mt-3 mr-3 ml-4"
       v-model="search"
     />
+    <select v-model="filterClass" class="input mr-3">
+          <option class="text-darkViolet" value="">Class</option>
+          <option class="text-darkViolet" value="Assassin">Assassin</option>
+          <option class="text-darkViolet" value="Duelist">Duelist</option>
+          <option class="text-darkViolet" value="Berserker">Berserker</option>
+          <option class="text-darkViolet" value="Bruiser">Bruiser</option>
+          <option class="text-darkViolet" value="Burst">Burst</option>
+          <option class="text-darkViolet" value="Battlemage">Battlemage</option>
+          <option class="text-darkViolet" value="Arcanist">Arcanist</option>
+          <option class="text-darkViolet" value="Guardian">Guardian</option>
+          <option class="text-darkViolet" value="Warlord">Warlord</option>
+          <option class="text-darkViolet" value="Enchanter">Enchanter</option>
+          <option class="text-darkViolet" value="Controller">Controller</option>
+          <option class="text-darkViolet" value="Marksman">Marksman</option>
+          <option class="text-darkViolet" value="Specialist">Specialist</option>
+    </select>
+    <select v-model="filterType" class="input mr-3">
+          <option class="text-darkViolet" value="">Attack Type</option>
+          <option class="text-darkViolet" value="Melee">Melee</option>
+          <option class="text-darkViolet" value="Range">Range</option>
+    </select>
     Total Heroes: {{ filteredHeroes.length }}
   </div>
   <div class="grid grid-cols-5 mb-5">
@@ -26,6 +47,8 @@ export default {
     return {
       search: "",
       likeOnly: false,
+      filterClass: "",
+      filterType: "",
     };
   },
   emits: [
@@ -56,15 +79,10 @@ export default {
   computed: {
     filteredHeroes() {
       let listBuffer = JSON.parse(JSON.stringify(this.heroList));
-      if (this.search == "") {
+      if (this.search && this.filterClass && this.filterType) {
         return listBuffer.sort(this.compare);
       } else {
-        return listBuffer
-          .filter(
-            (hero) =>
-              hero.name.toLowerCase().includes(this.search.toLowerCase())
-          )
-          .sort(this.compare);
+        return listBuffer.filter((hero) => hero.name.toLowerCase().includes(this.search.toLowerCase()) && hero.class.toLowerCase().includes(this.filterClass.toLowerCase()) && hero.attackType.toLowerCase().includes(this.filterType.toLowerCase())).sort(this.compare);
       }
     },
   },
